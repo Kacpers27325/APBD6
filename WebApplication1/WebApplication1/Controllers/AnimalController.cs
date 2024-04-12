6cb35134ae90231b9955cec4981aba284a48f81c
kacpers27325 <s27325@pjwstk.edu.pl>
2024-04-12 13:06:55 +0200

    a

---
+++ WebApplication1/WebApplication1/Controllers/AnimalController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class AnimalController:ControllerBase
+{
+
+    private readonly IConfiguration _configuration;
+
+    public AnimalController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        //otwieramy połączenie
+        using SqlConnection connection = new SqlConnection("");
+        connection.Open();
+        //Definiujemy query
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "Select * from Animals";
+        
+        //uruchomienie commanda
+        var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            
+        }
+        
+        return Ok();
+    }
+}
--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8cbe83a18c907df6311642dbd8d0209ba8ebe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dbdfeef977665dd205eeb61638d10880ddaecfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
